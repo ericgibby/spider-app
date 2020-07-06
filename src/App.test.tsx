07c01a27f302a1a25979496a95b2c94c9ebc5f0f
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-06 14:05:43 -0600

    Removed sample code from App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders spider heading', () => {
 	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+	const headingElement = screen.getByText(/spider/i);
+	expect(headingElement).toBeInTheDocument();
 });
