18bed5659ce2ad16201e9518236391d7bcc710e8
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-07 15:40:19 -0600

    Added TextInput to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from './components/Button/Button';
+import TextInput from './components/TextInput/TextInput';
 
 function App() {
 	return (
 		<div className="App container mx-auto">
 			<h1 className="text-5xl">Spider</h1>
 			<div className="my-8">
+				<TextInput label="Enter a word or phrase" />
 				<Button
 					onClick={() => {
 						alert('Hello world!');
