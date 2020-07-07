af1c555167a886ef31c74ee27bf96ba303fc79f2
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-07 15:37:33 -0600

    Added Button to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Button from './components/Button/Button';
 
 function App() {
 	return (
 		<div className="App container mx-auto">
 			<h1 className="text-5xl">Spider</h1>
+			<div className="my-8">
+				<Button
+					onClick={() => {
+						alert('Hello world!');
+					}}
+					text="OK"
+				></Button>
+			</div>
 		</div>
 	);
 }
