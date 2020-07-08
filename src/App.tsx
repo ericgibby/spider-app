7144a10bd4bf45d8c77e7dfeb0d407f286df9197
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-08 10:44:27 -0600

    Added navigation to /play route when form is submitted

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import PlayContainer from './containers/PlayContainer';
 import StartContainer from './containers/StartContainer';
 
 function App() {
+	const history = useHistory();
+
+	const handleSubmit = (value: string) => {
+		history.push('/play');
+	};
+
 	return (
 		<div className="App container mx-auto">
 			<h1 className="text-5xl">Spider</h1>
 			<div className="my-8">
 				<Switch>
 					<Route path="/start">
-						<StartContainer
-							onSubmit={(value: string) => {
-								alert(value);
-							}}
-						/>
+						<StartContainer onSubmit={handleSubmit} />
 					</Route>
 					<Route path="/play" component={PlayContainer} />
 					<Redirect path="*" to="/start" />
