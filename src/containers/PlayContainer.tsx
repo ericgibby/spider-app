f7687401fce26e941b09ed82623f1de320c7283f
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-08 15:53:54 -0600

    Added MaskedText component to PlayContainer

--- src/containers/PlayContainer.tsx
+++ src/containers/PlayContainer.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MaskedText from '../components/MaskedText/MaskedText';
 
-function PlayContainer() {
+type PlayContainerProps = {
+	text?: string;
+};
+
+function PlayContainer({ text }: PlayContainerProps) {
 	return (
-		<Link
-			className="text-blue-600 underline hover:text-blue-700"
-			to="/start"
-		>
-			Start Over
-		</Link>
+		<>
+			<Link
+				className="text-blue-600 underline hover:text-blue-700"
+				to="/start"
+			>
+				Start Over
+			</Link>
+			<MaskedText text={text} />
+		</>
 	);
 }
 
