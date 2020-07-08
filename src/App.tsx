6289619bb48895fd11b1f8984075593cc44a01d6
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-08 15:54:47 -0600

    Added state to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import PlayContainer from './containers/PlayContainer';
 import StartContainer from './containers/StartContainer';
@@ -6,7 +6,10 @@ import StartContainer from './containers/StartContainer';
 function App() {
 	const history = useHistory();
 
+	const [text, setText] = useState('');
+
 	const handleSubmit = (value: string) => {
+		setText(value);
 		history.push('/play');
 	};
 
@@ -18,7 +21,9 @@ function App() {
 					<Route path="/start">
 						<StartContainer onSubmit={handleSubmit} />
 					</Route>
-					<Route path="/play" component={PlayContainer} />
+					<Route path="/play">
+						<PlayContainer text={text} />
+					</Route>
 					<Redirect path="*" to="/start" />
 				</Switch>
 			</div>
