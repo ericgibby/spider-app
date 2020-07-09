1a39993a811b9e084a2cd847ca773ee43ab59d2e
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-08 23:46:05 -0600

    Don't allow step to exceed number of spider images

--- src/components/Spider/Spider.tsx
+++ src/components/Spider/Spider.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import spider00 from './spider-00.svg';
-import spider01 from './spider-01.svg';
-import spider02 from './spider-02.svg';
-import spider03 from './spider-03.svg';
-import spider04 from './spider-04.svg';
-import spider05 from './spider-05.svg';
-import spider06 from './spider-06.svg';
-import spider07 from './spider-07.svg';
-import spider08 from './spider-08.svg';
-import spider09 from './spider-09.svg';
-import spider10 from './spider-10.svg';
 
 const IMAGES = [
-	spider00,
-	spider01,
-	spider02,
-	spider03,
-	spider04,
-	spider05,
-	spider06,
-	spider07,
-	spider08,
-	spider09,
-	spider10
+	require('./spider-00.svg'),
+	require('./spider-01.svg'),
+	require('./spider-02.svg'),
+	require('./spider-03.svg'),
+	require('./spider-04.svg'),
+	require('./spider-05.svg'),
+	require('./spider-06.svg'),
+	require('./spider-07.svg'),
+	require('./spider-08.svg'),
+	require('./spider-09.svg'),
+	require('./spider-10.svg')
 ];
 
 type SpiderProps = {
@@ -30,7 +19,8 @@ type SpiderProps = {
 };
 
 function Spider({ step = 0 }: SpiderProps) {
-	const src = IMAGES[step];
+	const src =
+		step >= IMAGES.length ? IMAGES[IMAGES.length - 1] : IMAGES[step];
 	return (
 		<div className="Spider">
 			<img alt="Spider" src={src} />
