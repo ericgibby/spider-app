878f5789b54be39cc9b9dad4ac99bee6c16b3d26
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-09 14:53:36 -0600

    Re-built app

---
+++ precache-manifest.694f7c1d744381926f9acfc9d77c84ea.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9183c56f8fa5d434e5c7aa7373c6e12",
+    "url": "/spider-app/index.html"
+  },
+  {
+    "revision": "24e81fe73bf60e34709e",
+    "url": "/spider-app/static/css/main.fd4728bc.chunk.css"
+  },
+  {
+    "revision": "d9b95461bf40b84d0c0e",
+    "url": "/spider-app/static/js/2.63d8043b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spider-app/static/js/2.63d8043b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f53a6cdb667570242a",
+    "url": "/spider-app/static/js/3.1b004f53.chunk.js"
+  },
+  {
+    "revision": "24e81fe73bf60e34709e",
+    "url": "/spider-app/static/js/main.3d4f249c.chunk.js"
+  },
+  {
+    "revision": "61a79a727b9d9e9a24e7",
+    "url": "/spider-app/static/js/runtime-main.ec49a702.js"
+  },
+  {
+    "revision": "232ad789a80171e4ef5ff653b83791f1",
+    "url": "/spider-app/static/media/spider-00.232ad789.svg"
+  },
+  {
+    "revision": "94a5fbe95b14dd3986767fd5e1e176df",
+    "url": "/spider-app/static/media/spider-01.94a5fbe9.svg"
+  },
+  {
+    "revision": "5adff19f0036a35c6450f42f3a0178d8",
+    "url": "/spider-app/static/media/spider-02.5adff19f.svg"
+  },
+  {
+    "revision": "eae3122a40999f7cd3d521e2767804b9",
+    "url": "/spider-app/static/media/spider-03.eae3122a.svg"
+  },
+  {
+    "revision": "431a4e589281f00bc3887b58b4ddf3bf",
+    "url": "/spider-app/static/media/spider-04.431a4e58.svg"
+  },
+  {
+    "revision": "81454eb117d11bbaa889befba1f9187b",
+    "url": "/spider-app/static/media/spider-05.81454eb1.svg"
+  },
+  {
+    "revision": "442a6ca9cb596b1d7ede35be702d01d1",
+    "url": "/spider-app/static/media/spider-06.442a6ca9.svg"
+  },
+  {
+    "revision": "cd612159132ac254c6e50dd128979657",
+    "url": "/spider-app/static/media/spider-07.cd612159.svg"
+  },
+  {
+    "revision": "f3da250714780c5e64e24b594ace8c23",
+    "url": "/spider-app/static/media/spider-08.f3da2507.svg"
+  },
+  {
+    "revision": "c0e9d0284e5c996cbe60395563335d40",
+    "url": "/spider-app/static/media/spider-09.c0e9d028.svg"
+  },
+  {
+    "revision": "6a37e96e8514234c648de01a74875726",
+    "url": "/spider-app/static/media/spider-10.6a37e96e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3693cb35ff75000fabe1698ec781568.js"
+  "/spider-app/precache-manifest.694f7c1d744381926f9acfc9d77c84ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spider-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
