a477f3bea2b65f51b511c8e3592e99371a51e1ac
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-09 13:43:13 -0600

    Disable letter buttons when game is complete

--- src/containers/PlayContainer.tsx
+++ src/containers/PlayContainer.tsx
@@ -104,6 +104,7 @@ function PlayContainer({ text }: PlayContainerProps) {
 			<div className="grid gap-6 grid-cols-1 md:grid-cols-2">
 				<Spider step={step} />
 				<LetterButtons
+					disabled={winner || loser}
 					onClick={handleClick}
 					text={text}
 					usedLetters={usedLetters}
