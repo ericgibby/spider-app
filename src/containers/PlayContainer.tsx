7251fa4788b4bbe3042579832c1f767c86bd3547
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-09 09:54:26 -0600

    Added winner/loser notifications

--- src/containers/PlayContainer.tsx
+++ src/containers/PlayContainer.tsx
@@ -22,13 +22,20 @@ function PlayContainer({ text }: PlayContainerProps) {
 		[text]
 	);
 
-	const incorrectCount = useMemo(
+	const [correctCount, incorrectCount] = useMemo(
 		() =>
-			usedLetters.reduce((count, letter) => {
-				return !textLetters[letter] ? count + 1 : count;
-			}, 0),
+			usedLetters.reduce(
+				([correct, incorrect], letter) =>
+					textLetters[letter]
+						? [correct + 1, incorrect]
+						: [correct, incorrect + 1],
+				[0, 0]
+			),
 		[textLetters, usedLetters]
 	);
+
+	const loser = incorrectCount === MAX_INCORRECT;
+	const winner = correctCount === Object.keys(textLetters).length;
 
 	const handleClick = (letter: string) => {
 		setUsedLetters(previous => [...previous, letter]);
@@ -42,7 +49,23 @@ function PlayContainer({ text }: PlayContainerProps) {
 			>
 				Start Over
 			</Link>
-			<div className="my-6 py-4 border-b border-gray-500">
+			{loser && (
+				<div className="p-6 bg-red-200 border border-red-500 rounded-md">
+					Sorry. You lost.{' '}
+					<span role="img" aria-label="sad emoji">
+						😢
+					</span>
+				</div>
+			)}
+			{winner && (
+				<div className="p-6 bg-blue-200 border border-blue-500 rounded-md">
+					Congratulations! You won!{' '}
+					<span role="img" aria-label="smiley emoji">
+						😁
+					</span>
+				</div>
+			)}
+			<div className="mb-6 py-4 border-b border-gray-500 text-center">
 				<MaskedText text={text} usedLetters={usedLetters} />
 			</div>
 			<div className="grid gap-6 grid-cols-1 md:grid-cols-2">
