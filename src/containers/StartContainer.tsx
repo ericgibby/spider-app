35d281e4e735afb601e33754c0d54446f0a807bf
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-14 15:16:23 -0600

    Updated StartContainer to get state from Redux

--- src/containers/StartContainer.tsx
+++ src/containers/StartContainer.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import TextInputForm, {
-	TextInputFormProps
-} from '../components/TextInputForm/TextInputForm';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import TextInputForm from '../components/TextInputForm/TextInputForm';
+import { setText } from '../redux/modules/play';
+import { selectText } from '../redux/selectors/play';
 
-function StartContainer(props: TextInputFormProps) {
+function StartContainer() {
+	const dispatch = useDispatch();
+	const history = useHistory();
+
+	const text = useSelector(selectText);
+
+	useEffect(() => {
+		if (text) {
+			history.push('/play');
+		}
+	}, [history, text]);
+
+	const handleSubmit = (text: string) => {
+		dispatch(setText(text));
+	};
+
 	return (
 		<>
 			<p className="mb-2">
 				Enter a word or phrase for other players to guess.
 			</p>
-			<TextInputForm {...props} />
+			<TextInputForm onSubmit={handleSubmit} />
 		</>
 	);
 }
