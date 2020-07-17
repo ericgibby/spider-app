a2b90a4223dc44991555b7d94d4dfba3d49a8eec
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-17 14:58:41 -0600

    Persist play state to local storage

--- src/redux/root.ts
+++ src/redux/root.ts
@@ -1,9 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { combineEpics, Epic } from 'redux-observable';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import play, { setTextEpic } from './modules/play';
 
 export const rootEpic = combineEpics(setTextEpic) as Epic;
 
-const rootReducer = combineReducers({ play });
+const rootReducer = combineReducers({
+	play: persistReducer({ key: 'play', storage }, play)
+});
 
 export default rootReducer;
