b17a38dbabba190ede014539fe38b0d20ae31964
Eric Gibby <eric.gibby@imaginelearning.com>
2020-07-17 14:59:48 -0600

    Update middleware config to not check if persist actions are serializable

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -2,7 +2,15 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { createEpicMiddleware } from 'redux-observable';
 import reducer, { rootEpic } from './root';
 
-const defaultMiddleware = getDefaultMiddleware();
+const defaultMiddleware = getDefaultMiddleware({
+	serializableCheck: {
+		ignoredActions: [
+			// 👇 These are from a 3rd party library, so we'll just have to ignore them.
+			'persist/PERSIST',
+			'persist/REHYDRATE'
+		]
+	}
+});
 const epicMiddleware = createEpicMiddleware();
 const middleware = [...defaultMiddleware, epicMiddleware];
 
